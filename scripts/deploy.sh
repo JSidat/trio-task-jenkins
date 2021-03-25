2e752bd5e27f18ca167bb4392ae7ad7fb1966536
JSidat <64256367+JSidat@users.noreply.github.com>
2021-03-25 11:34:05 +0000

    Update deploy.sh

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -4,6 +4,6 @@ sudo docker rmi $(docker images -aq)
 
 docker-compose up -d
 
-docker push junaidsidat95/trio-task:flask-app
-docker push junaidsidat95/trio-task:mysql
+docker push junaidsidat95/trio-task-flask-app
+docker push junaidsidat95/trio-task-mysql
 
